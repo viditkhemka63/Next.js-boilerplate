d6a8c3e53d4eb57876d770793e89585191c621c5
viditkhemka63 <viditkhemka63@gmail.com>
2022-02-07 00:13:16 +0530

    feat: adding emotion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,47 @@
+import Head from 'next/head';
 import { AppProps } from 'next/app';
-import '@/styles/global.scss';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
+
 import { Layout } from '@/components/layouts/layout';
+import { lightTheme } from '@/utils/theme';
 
-export function reportWebVitals(metric: any) {
-  console.log(`metic is `, metric);
-}
+import '@/styles/global.scss';
+import createEmotionCache from '@/utils/createEmotionCache';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-type CustomAppProps = AppProps & {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+type MyAppProps = AppProps & {
   Component: { getLayout: (page: ReactNode) => ReactNode };
+  emotionCache?: EmotionCache;
 };
 
-export default function MyApp({ Component, pageProps }: CustomAppProps) {
+export default function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) {
+  const [themeState, setThemeState] = useState('light');
   const getLayout =
     Component.getLayout ?? ((page: ReactNode) => <Layout>{page}</Layout>);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={lightTheme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export function reportWebVitals(metric: any) {
+  console.log(`metic is `, metric);
 }
---
+++ src/utils/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+// Create a theme instance.
+const lightTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
+
+export { lightTheme };
